660e766610ba3df59ec31a58558b217fadd2f7c1
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-08-30 21:11:34 +0700

    update auth jwt token

---
+++ src/App.Services.AuthAPI/Services/IServices/IJwtTokenGenerator.cs
@@ -0,0 +1,9 @@
+using App.Services.AuthAPI.Models;
+
+namespace App.Services.AuthAPI.Service.IService
+{
+    public interface IJwtTokenGenerator
+    {
+        string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles);
+    }
+}
---
+++ src/App.Services.AuthAPI/Services/JwtTokenGenerator.cs
@@ -0,0 +1,45 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using App.Services.AuthAPI.Models;
+using App.Services.AuthAPI.Service.IService;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace App.Services.AuthAPI.Services
+{
+    public class JwtTokenGenerator : IJwtTokenGenerator
+    {
+        private readonly JwtOptions _jwtOptions;
+        private readonly IConfiguration _configuration;
+
+        public JwtTokenGenerator(IOptions<JwtOptions> jwtOptions, IConfiguration configuration)
+        {
+            _jwtOptions = jwtOptions.Value;
+            _configuration = configuration;
+        }
+
+        public string GenerateToken(ApplicationUser applicationUser, IEnumerable<string> roles)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Email, applicationUser.Email),
+                new Claim(JwtRegisteredClaimNames.Sub, applicationUser.Id),
+                new Claim(JwtRegisteredClaimNames.Name, applicationUser.Name),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_jwtOptions.Key));
+            var handler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Audience = _jwtOptions.Audience,
+                Issuer = _jwtOptions.Issuer,
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = handler.CreateToken(tokenDescriptor);
+            return handler.WriteToken(token);
+        }
+    }
+}
