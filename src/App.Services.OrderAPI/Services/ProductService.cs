0371e8b4a64f2e23eecd20f5b8e07b3e4b583a0c
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-10-02 19:20:47 +0700

    update order api

---
+++ src/App.Services.OrderAPI/Services/ProductService.cs
@@ -0,0 +1,28 @@
+using App.Services.OrderAPI.Models;
+using App.Services.OrderAPI.Services.IServices;
+using Newtonsoft.Json;
+
+namespace App.Services.OrderAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public ProductService(IHttpClientFactory clientFactory)
+        {
+            _httpClientFactory = clientFactory;
+        }
+        public async Task<IEnumerable<Product>> Get()
+        {
+            var client = _httpClientFactory.CreateClient("Product");
+            var response = await client.GetAsync($"/api/product");
+            var apiContent = await response.Content.ReadAsStringAsync();
+            var resp = JsonConvert.DeserializeObject<Response>(apiContent);
+            if (resp.IsSuccess)
+            {
+                return JsonConvert.DeserializeObject<IEnumerable<Product>>(Convert.ToString(resp.Result));
+            }
+            return new List<Product>();
+        }
+    }
+}
