91ecb1ec36d02e899da403e1ea1967a22d83f68f
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-10-02 19:20:39 +0700

    update product api

--- src/App.Services.ProductAPI/Models/Product.cs
+++ src/App.Services.ProductAPI/Models/Product.cs
@@ -9,19 +9,24 @@ public class Product
     public double Price { get; set; }
     public string? ImageUrl { get; set; }
     public string? ImageLocalPath { get; set; }
-    [NotMapped, SwaggerIgnore]
-    public IFormFile? Image { get; set; }
     public int SizeId { get; set; }
     public int ColorId { get; set; }
     public int CategoryId { get; set; }
     public int BrandId { get; set; }
 
     [NotMapped, SwaggerIgnore]
+    public IFormFile? Image { get; set; }
+
+
+    [NotMapped, SwaggerIgnore]
     public virtual Size Size { get; set; }
+
     [NotMapped, SwaggerIgnore]
     public virtual Color Color { get; set; }
+
     [NotMapped, SwaggerIgnore]
     public virtual Category Category { get; set; }
+
     [NotMapped, SwaggerIgnore]
     public virtual Brand Brand { get; set; }
 }
