8743cbc2a9588da76d471c93d1e5dcf944f99d4d
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-10-26 12:05:02 +0700

    remove template project

--- src/template/Models/Dto/Pagination.cs
+++
@@ -1,7 +0,0 @@
-namespace template.Models;
-public class Pagination
-{
-    public IEnumerable<Product> Products { get; set; }
-    public int totalPages { get; set; }
-    public int currentPage { get; set; }
-}
