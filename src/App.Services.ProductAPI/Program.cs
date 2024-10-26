e8de6847584774ebfbc09817e9e50342bb6be635
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-10-26 11:04:08 +0700

    update remove controller v2 ( fix 500.30)

--- src/App.Services.ProductAPI/Program.cs
+++ src/App.Services.ProductAPI/Program.cs
@@ -59,11 +59,6 @@ builder.Services.AddSwaggerGen(option =>
             Url = new Uri("https://documenter.getpostman.com/view/33236192/2sAXxV5pNK")
         },
     });
-    option.SwaggerDoc("v2", new OpenApiInfo
-    {
-        Version = "v2.0",
-        Title = "App.Services.ProductAPI",
-    });
 });
 
 builder.AddAppAuthetication();
@@ -77,7 +72,6 @@ app.UseSwaggerUI(options =>
         app.UseSwaggerUI(options =>
         {
             options.SwaggerEndpoint("/swagger/v1/swagger.json", "App.Services.ProductAPI V1");
-            options.SwaggerEndpoint("/swagger/v2/swagger.json", "App.Services.ProductAPI V2");
         });
     }
     else
@@ -85,7 +79,6 @@ app.UseSwaggerUI(options =>
         app.UseSwaggerUI(options =>
         {
             options.SwaggerEndpoint("/swagger/v1/swagger.json", "App.Services.ProductAPI V1");
-            options.SwaggerEndpoint("/swagger/v2/swagger.json", "App.Services.ProductAPI V2");
             options.RoutePrefix = string.Empty;
         });
     }
