377057276f0f7f811bd68c5815d3f7fe85b16fee
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-11-20 07:58:36 +0700

    update router

--- src/domain/App.Frontend/Areas/Account/Controllers/AuthenticationController.cs
+++ src/domain/App.Frontend/Areas/Account/Controllers/AuthenticationController.cs
@@ -14,6 +14,7 @@ using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 
 namespace App.Frontend.Areas.Account.Controllers
 {
+    [Area("Account")]
     [Route("{controller}/{action}")]
     public class AuthenticationController : Controller
     {
