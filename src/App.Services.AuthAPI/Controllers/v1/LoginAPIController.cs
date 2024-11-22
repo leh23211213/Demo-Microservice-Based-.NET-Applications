e79aa605471da590bcc0d909c9c4d83085715a1e
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-11-22 12:32:58 +0700

    update validate parameter

--- src/App.Services.AuthAPI/Controllers/v1/LoginAPIController.cs
+++ src/App.Services.AuthAPI/Controllers/v1/LoginAPIController.cs
@@ -41,31 +41,32 @@ namespace App.Services.AuthAPI.Controllers
         [ValidateAntiForgeryToken]
         public async Task<ActionResult<Response>> Login([FromBody] LoginRequest model)
         {
-            try
+            if (model is null) return _response;
+
+            if (ModelState.IsValid)
             {
-                if (ModelState.IsValid)
+                try
                 {
+                    // create token
                     var token = await _authAPIService.Login(model);
                     if (token == null || string.IsNullOrEmpty(token.AccessToken))
                     {
-                        _response.IsSuccess = false;
                         _response.Message = "Email or password is incorrect";
-                        _response.StatusCode = HttpStatusCode.BadRequest;
                         return _response;
                     }
-
+                    // Validate token
                     if (Validate(token.AccessToken))
                     {
                         _response.Result = token;
                         _tokenProvider.SetToken(token);
                     }
                 }
-            }
-            catch (Exception ex)
-            {
-                _response.IsSuccess = false;
-                _response.Message = ex.Message;
-                _response.StatusCode = HttpStatusCode.BadRequest;
+                catch (Exception ex)
+                {
+                    _response.IsSuccess = false;
+                    _response.Message = "Internal Server Error";
+                    _response.StatusCode = HttpStatusCode.InternalServerError;
+                }
             }
             return _response;
         }
@@ -90,18 +91,18 @@ namespace App.Services.AuthAPI.Controllers
 
                 return true;
             }
-            catch (SecurityTokenExpiredException)
-            {
-                new SecurityTokenExpiredException("Token has expired.");
-            }
-            catch (SecurityTokenNotYetValidException)
-            {
-                new SecurityTokenNotYetValidException("Token is not yet valid.");
-            }
-            catch (SecurityTokenInvalidSignatureException)
-            {
-                new SecurityTokenInvalidSignatureException("Invalid signature.");
-            }
+            // catch (SecurityTokenExpiredException)
+            // {
+            //     new SecurityTokenExpiredException("Token has expired.");
+            // }
+            // catch (SecurityTokenNotYetValidException)
+            // {
+            //     new SecurityTokenNotYetValidException("Token is not yet valid.");
+            // }
+            // catch (SecurityTokenInvalidSignatureException)
+            // {
+            //     new SecurityTokenInvalidSignatureException("Invalid signature.");
+            // }
             catch (Exception ex)
             {
                 new Exception(ex.Message);
