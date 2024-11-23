9bf61015bf40f4b2aef985583174356fafad5633
Hiep Le <83544494+leh23211213@users.noreply.github.com>
2024-11-23 13:07:14 +0700

    update conflict auth api

--- src/App.Services.AuthAPI/Controllers/v1/LoginAPIController.cs
+++ src/App.Services.AuthAPI/Controllers/v1/LoginAPIController.cs
@@ -2,6 +2,9 @@ using System.Net;
 using Microsoft.AspNetCore.Mvc;
 using App.Services.AuthAPI.Models;
 using App.Services.AuthAPI.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
 
 namespace App.Services.AuthAPI.Controllers
 {
@@ -11,33 +14,101 @@ namespace App.Services.AuthAPI.Controllers
     public class LoginAPIController : ControllerBase
     {
         protected Response _response;
+        private readonly string issuer;
+        private readonly string audience;
+        private readonly string secretKey;
         private readonly IAuthAPIService _authAPIService;
-
+        private readonly ITokenProvider _tokenProvider;
+        private readonly IConfiguration _configuration;
         public LoginAPIController(
-                                IAuthAPIService authAPIService
+                                IConfiguration configuration,
+                                IAuthAPIService authAPIService,
+                                ITokenProvider tokenProvider
                                 )
         {
             _response = new();
             _authAPIService = authAPIService;
+            _tokenProvider = tokenProvider;
+
+            _configuration = configuration;
+
+            issuer = _configuration.GetValue<string>("SetToken:Issuer");
+            audience = _configuration.GetValue<string>("SetToken:Audience");
+            secretKey = _configuration.GetValue<string>("SetToken:Secret");
         }
 
         [HttpPost("Login")]
         [ValidateAntiForgeryToken]
         public async Task<ActionResult<Response>> Login([FromBody] LoginRequest model)
         {
+            if (model is null) return _response;
+
             if (ModelState.IsValid)
             {
-                var token = await _authAPIService.Login(model);
-                if (token == null || string.IsNullOrEmpty(token.AccessToken))
+                try
+                {
+                    // create token
+                    var token = await _authAPIService.Login(model);
+                    if (token == null || string.IsNullOrEmpty(token.AccessToken))
+                    {
+                        _response.Message = "Email or password is incorrect";
+                        return _response;
+                    }
+                    // Validate token
+                    if (Validate(token.AccessToken))
+                    {
+                        _response.Result = token;
+                        _tokenProvider.SetToken(token);
+                    }
+                }
+                catch (Exception ex)
                 {
                     _response.IsSuccess = false;
-                    _response.Message = "Email or password is incorrect";
-                    _response.StatusCode = HttpStatusCode.BadRequest;
-                    return _response;
+                    _response.Message = "Internal Server Error";
+                    _response.StatusCode = HttpStatusCode.InternalServerError;
                 }
-                _response.Result = token;
             }
             return _response;
         }
+
+        private bool Validate(string ac_tk)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.UTF8.GetBytes(secretKey);
+            try
+            {
+                // Validate token
+                var principal = tokenHandler.ValidateToken(ac_tk, new TokenValidationParameters
+                {
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ClockSkew = TimeSpan.Zero
+                }, out SecurityToken validatedToken);
+
+                return true;
+            }
+            // catch (SecurityTokenExpiredException)
+            // {
+            //     new SecurityTokenExpiredException("Token has expired.");
+            // }
+            // catch (SecurityTokenNotYetValidException)
+            // {
+            //     new SecurityTokenNotYetValidException("Token is not yet valid.");
+            // }
+            // catch (SecurityTokenInvalidSignatureException)
+            // {
+            //     new SecurityTokenInvalidSignatureException("Invalid signature.");
+            // }
+            catch (Exception ex)
+            {
+                new Exception(ex.Message);
+            }
+            return false;
+        }
+
     }
 }
